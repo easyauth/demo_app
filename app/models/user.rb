b5addd1a63feb5dde3e5092fc633160a0182ff3e
demize <demize@unstable.systems>
2017-08-17 19:08:59 -0400

    Prevent Jeff's abuse :(

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+	validates_presence_of :easyauth_uid, message: "Are you using TLS?"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816220114) do
+ActiveRecord::Schema.define(version: 20170817230749) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170816220114) do
     t.integer "easyauth_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
